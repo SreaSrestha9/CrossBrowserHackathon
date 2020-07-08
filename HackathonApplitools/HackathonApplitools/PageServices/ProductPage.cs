b093d0ecd838a42b17a8485d8e40f42df75b99e0
Shreya Shrestha <Shreya.Shrestha@verscend.com>
2020-07-08 12:29:21 +0545

    added original solution folder

---
+++ HackathonApplitools/HackathonApplitools/PageServices/ProductPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HackathonApplitools.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace HackathonApplitools.PageServices
+{
+    public class ProductPage
+    {
+
+        public IWebDriver Driver { get { return SiteDriver.Driver; } }
+        private static IJavaScriptExecutor _javascriptexecutor;
+
+        #region Public Methods
+
+        public bool IsPageHeaderPresent() =>
+            SiteDriver.IsElementPresent(ProductDetailPageObject.PageHeaderCssSelector, How.CssSelector);
+
+        #endregion
+    }
+}
